976247d94209cc4d521a949c23342e0d54443dda
GustavoWustemberg <gustavowustemberg14@gmail.com>
2023-04-16 03:05:07 -0300

    adjust style in page 'Nossos Anjos'

--- js/script.js
+++ js/script.js
@@ -59,7 +59,8 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
+  #testimonials header, #testimonials .testimonials,
+  #ourAngels header, #gallery .gallery-item,
   #contact .text, #contact .links,
   footer .brand, footer .social
   `,
